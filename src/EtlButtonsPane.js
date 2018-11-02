69ea41468c48ccd4bb7bf7ee8b5f8e246fde4322
brq0 <bartoszturek0@gmail.com>
2018-11-02 18:44:49 +0100

    POP-UP. Fix previous popup. Try to send request every 300 ms, but doesn't stop. Need fix.

--- src/EtlButtonsPane.js
+++ src/EtlButtonsPane.js
@@ -2,9 +2,10 @@ import React, {Component} from 'react';
 import axios from "axios/index";
 import Button from './Button.js'
 import {Button as RSButton} from 'reactstrap';
+import $ from 'jquery';
 import * as CONF from './conf/Conf.js';
-import Popup from "reactjs-popup";
 import './App.css';
+import './css/popUp.css'
 
 
 class EtlButtonsPane extends Component {
@@ -15,26 +16,84 @@ class EtlButtonsPane extends Component {
         this.handleTransform = this.handleTransform.bind(this);
         this.handleLoad = this.handleLoad.bind(this);
         this.sendRequest = this.sendRequest.bind(this);
+
+        this.state = {
+            log: "nothing"
+        };
     }
 
     handleExtract(e){
        console.log("extract")
-       this.sendRequest("extract")
+       var k = setInterval(() => {
+
+            this.sendRequest("extract")
+
+             if(this.state.log === "Done"){
+                   console.log("stop")
+                   return;
+      }
+//         if(this.state.log !== "Data extracted successfully."){
+//             this.sendRequest("extract")
+//         }else{
+//                      clearInterval();
+//                      return;
+//                   }
+       }, 300);
+       if(this.state.log === "Done"){
+       console.log("stop")
+                       clearInterval(k);
+                  }
     }
 
     handleTransform(e){
       console.log("transform")
-      this.sendRequest("transform")
+      setInterval(() => {
+
+           this.sendRequest("transform")
+
+           if(this.state.log === "Done"){
+                return;
+           }
+//         if(this.state.log !== "Data transformed successfully."){
+//             this.sendRequest("transform")
+//         }else{
+//            clearInterval();
+//            return;
+//         }
+       }, 300);
     }
 
     handleLoad(e){
       console.log("load")
-      this.sendRequest("load")
+      setInterval(() => {
+         if(this.state.log !== "done"){
+             this.sendRequest("load")
+         }else return
+       }, 300);
     }
 
     sendRequest(command){
+    console.log(command)
      axios.get(`${CONF.PAGE}/${command}`)
           .then(({data}) => {
+            switch(data){
+                case "Data is being extracted..":
+                case "Data is being transformed..":
+                case "Data is being loaded..":
+                     $("#popup").show();
+                     this.setState({
+                        log: "running"
+                     })
+                     break;
+
+                default:
+                     $("#popup").hide();
+                     this.setState({
+                        log: "Done"
+                     })
+                     break;
+            }
+
            {this.props.handleLogging(data)}
      })
      .catch(error => {
@@ -46,35 +105,27 @@ class EtlButtonsPane extends Component {
         return (
             <div className="EtlButtonsPane">
                <div className="col-md-9 mt-5 col-centered">
-               <Popup
-                    trigger={<RSButton className="col-1" outline color="warning" onClick={this.handleExtract}>E</RSButton>} modal >
-                    
-
-        <div className="content">
-          Extracting data
-
-        <div id="wrapper">
-            <div class="profile-main-loader">
-            <div class="loader">
-                <svg class="circular-loader"viewBox="25 25 50 50" >
-                <circle class="loader-path" cx="50" cy="50" r="20" fill="none" stroke="#70c542" stroke-width="2" />
-                </svg>
-            </div>
-            </div>  
-        </div>
-        
-        <div className="actions">
-         
-          
-        </div>
-        
-      </div>
-               
-                    
-                </Popup>
+                  <RSButton className="col-1" outline color="warning" onClick={this.handleExtract}>E</RSButton>
                   <RSButton className="col-1 ml-4" outline color="danger" onClick={this.handleTransform}>T</RSButton>
                   <RSButton className="col-1 ml-4" outline color="info" onClick={this.handleLoad}>L</RSButton>
                 </div>
+
+                <div id="popup" className="actionWindow">
+                    <div className="actionWindow-content">
+                        <div className="actionWindow-body">
+                            <div id="wrapper">
+                               <div className="profile-main-loader">
+                                <div className="loader">
+                                   <svg className="circular-loader"viewBox="25 25 50 50" >
+                                    <circle className="loader-path" cx="50" cy="50" r="20" fill="none" stroke="#70c542" strokeWidth="2" />
+                                   </svg>
+                                 </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         );
     }
