94a910b32befe7c6e40dbac2c8107580a0944deb
shiket <shiket95@gmail.com>
2018-11-02 13:46:42 +0100

    loading circle when extracting data

--- src/EtlButtonsPane.js
+++ src/EtlButtonsPane.js
@@ -2,8 +2,10 @@ import React, {Component} from 'react';
 import axios from "axios/index";
 import Button from './Button.js'
 import {Button as RSButton} from 'reactstrap';
-import * as CONF from './conf/Conf.js'
+import * as CONF from './conf/Conf.js';
+import Popup from "reactjs-popup";
 import './App.css';
+
 
 class EtlButtonsPane extends Component {
     constructor(props) {
@@ -44,7 +46,32 @@ class EtlButtonsPane extends Component {
         return (
             <div className="EtlButtonsPane">
                <div className="col-md-9 mt-5 col-centered">
-                  <RSButton className="col-1" outline color="warning" onClick={this.handleExtract}>E</RSButton>
+               <Popup
+                    trigger={<RSButton className="col-1" outline color="warning" onClick={this.handleExtract}>E</RSButton>} modal >
+                    
+
+        <div className="content">
+          Extracting data
+
+        <div id="wrapper">
+            <div class="profile-main-loader">
+            <div class="loader">
+                <svg class="circular-loader"viewBox="25 25 50 50" >
+                <circle class="loader-path" cx="50" cy="50" r="20" fill="none" stroke="#70c542" stroke-width="2" />
+                </svg>
+            </div>
+            </div>  
+        </div>
+        
+        <div className="actions">
+         
+          
+        </div>
+        
+      </div>
+               
+                    
+                </Popup>
                   <RSButton className="col-1 ml-4" outline color="danger" onClick={this.handleTransform}>T</RSButton>
                   <RSButton className="col-1 ml-4" outline color="info" onClick={this.handleLoad}>L</RSButton>
                 </div>
