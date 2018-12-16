0cd404efa0cd63fa840d89d138afdd4f7e1cc801
Shiket <shiket95@gmail.com>
2018-12-16 14:41:02 +0100

    remove unnecessary functions

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,20 +4,6 @@ Navbar,
 NavbarBrand} from 'reactstrap';
 
 class Header extends React.Component {  
-        constructor(props) {
-          super(props);
-      
-          this.toggle = this.toggle.bind(this);
-          this.state = {
-            isOpen: false
-          };
-        }
-        toggle() {
-          this.setState({
-            isOpen: !this.state.isOpen
-          });
-        }
-
     render() {
         return (
             <div className="header">
